39147f8d6f392e45742a355d7f2f20054bd2a1b9
daviddan-241 <ovourcross@gmail.com>
2025-12-10 21:02:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-echo 'import { defineConfig } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { nodePolyfills } from "vite-plugin-node-polyfills"
 
@@ -31,5 +31,5 @@ export default defineConfig({
       }
     }
   }
-})' > vite.config.js
+})
 
